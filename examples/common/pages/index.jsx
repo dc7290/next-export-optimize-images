5dfea168161fd6676f04ac16b2c439675d1622fb
dc7290 <dhkh.cba0927@gmail.com>
2022-05-10 03:20:35 +0900

    version 0.2.4

--- examples/common/pages/index.jsx
+++ examples/common/pages/index.jsx
@@ -7,7 +7,7 @@ const IndexPage = () => {
     <div style={{ width: '85vw', maxWidth: '960px', margin: '0 auto' }}>
       <Image src={imgSrc} priority />
 
-      <Image src="/img.png" width={1920} height={1281} sizes="(min-width: 768px) 720px, 85vw" />
+      <Image src="/images/img.png" width={1920} height={1281} sizes="(min-width: 768px) 720px, 85vw" />
     </div>
   )
 }
