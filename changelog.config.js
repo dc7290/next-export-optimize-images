8115b899ac45d2c2f6c315b050e153515b33ef7e
dc7290 <dhkh.cba0927@gmail.com>
2022-05-17 12:40:51 +0900

    ci(other): 🎡 git-czの導入
    git commit した時に自動的に git-cz が作動するように変更

---
+++ changelog.config.js
@@ -0,0 +1,58 @@
+/**
+ * This is a configuration for git-cz.
+ *
+ * @see {@link https://github.com/streamich/git-cz#custom-config} for documentation
+ * @see {@link https://github.com/streamich/git-cz/blob/master/lib/defaults.js} for default configs.
+ */
+
+const types = require('./commit-types.config')
+
+module.exports = {
+  /**
+   * コミットメッセージに絵文字を含めないか
+   */
+  disableEmoji: false,
+  /**
+   * コミットメッセージの題目の書式
+   */
+  format: "{type}{scope}: {emoji}{subject}",
+  /**
+   * コミット時に選択可能な型
+   */
+  list: types.map(({ type }) => type),
+  /**
+   * コミットメッセージ最大文字数
+   */
+  // maxMessageLength: 64,
+  /**
+   * コミットメッセージ最小文字数
+   */
+  // minMessageLength: 3,
+  /**
+   * コミット時に入力する項目
+   */
+  questions: [
+    "type", // 型
+    "scope", // スコープ
+    "subject", // コミットの題名
+    "body", // コミットの本文
+    "breaking", // breaking changeの内容
+    "issues", // クローズするGitHub issues
+  ],
+  /**
+   * スコープの選択肢。
+   */
+  scopes: ['components', 'plugin', 'cli', 'other'],
+  /**
+   * 各型の設定
+   */
+  types: Object.fromEntries(types.map(({ type, description, emoji }) => [type, { description, emoji, value: type }])),
+  /**
+   * BREAKING CHANGEに表示する絵文字
+   */
+  breakingChangePrefix: "🧨",
+  /**
+   * Closesに表示する絵文字
+   */
+  closedIssuePrefix: "✅",
+}
---
+++ commit-types.config.js
@@ -0,0 +1,102 @@
+/**
+ * @typedef {Object} Type conventional commitsの型の設定
+ * @property {string} type conventional commitsの型
+ * @property {"major" | "minor" | "patch" | undefined } [release] セマンティックバージョンの上げ方。undefinedはリリースされません。
+ * @property {string} description git-czに表示される説明文
+ * @property {string} emoji git-czで使う絵文字
+ * @property {string | undefined} [section] changelogに表示する見出し
+ * @property {boolean | undefined} [hidden] changelogに表示するか否か
+ */
+
+/**
+ * @type {Array.<Type>}
+ */
+module.exports = [
+  {
+    type: "feat", // 機能追加
+    release: "minor",
+    description: "A new feature",
+    emoji: "🚀",
+    section: "Features",
+    hidden: false,
+  },
+  {
+    type: "fix", // バグ修正
+    release: "patch",
+    description: "A bug fix",
+    emoji: "🐛",
+    section: "Bug Fixes",
+    hidden: false,
+  },
+  {
+    type: "sec", // 脆弱性の解消
+    release: "patch",
+    description: "A vulnerability fix",
+    emoji: "👮‍",
+    section: "Security",
+    hidden: false,
+  },
+  {
+    type: "perf", // パフォーマンスのみの改善
+    release: "patch",
+    description: "A code change that improves performance",
+    emoji: "⚡️",
+    section: "Performance Improvements",
+    hidden: false,
+  },
+  {
+    type: "refactor", // 機能追加やバグ修正を伴わないリファクタリング
+    release: undefined,
+    description: "A code change that neither fixes a bug or adds a feature",
+    emoji: "💡",
+    section: "Code Refactoring",
+    hidden: false,
+  },
+  {
+    type: "docs", // ドキュメントのみの変更
+    release: undefined,
+    description: "Documentation only changes",
+    emoji: "✏️",
+    section: `Documentation`,
+    hidden: false,
+  },
+  {
+    type: "release", // リリースコミット
+    release: undefined,
+    description: "Create a release commit",
+    emoji: "🏹",
+    hidden: true,
+  },
+  {
+    type: "style", // コーディングスタイル関連の修正
+    release: undefined,
+    description: "Markup, white-space, formatting, missing semi-colons...",
+    emoji: "💄",
+    section: "Styles",
+    hidden: false,
+  },
+  {
+    type: "test", // テストの追加変更
+    release: undefined,
+    description: "Adding missing tests",
+    emoji: "💍",
+    section: "Tests",
+    hidden: false,
+  },
+  {
+    type: "ci", // CI関連の変更
+    release: undefined,
+    description: "CI related changes",
+    emoji: "🎡",
+    section: `Continuous Integration`,
+    hidden: false,
+  },
+  {
+    type: "chore", // ビルドプロセスや補助ツールの変更
+    release: undefined,
+    description: "Build process or auxiliary tool changes",
+    emoji: "🤖",
+    section: `Chore`,
+    hidden: true,
+  },
+];
