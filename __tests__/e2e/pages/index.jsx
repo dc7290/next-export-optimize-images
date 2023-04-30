8cb56ca7c2c4f4d32a19f1a172015f5af86225bc
dc7290 <dhkh.cba0927@gmail.com>
2023-04-30 13:39:46 +0900

    refactor: 💡 typo

--- __tests__/e2e/pages/index.jsx
+++ __tests__/e2e/pages/index.jsx
@@ -28,7 +28,7 @@ const IndexPage = () => {
         {/* External image */}
         <Image src="https://next-export-optimize-images.vercel.app/og.png" width={1920} height={1280} alt="" />
 
-        {/* Animted image */}
+        {/* Animated image */}
         <Image src="/images/animated.webp" width={400} height={400} alt="" />
 
         {isClient && <Image src={clientOnlySrc} alt="" />}
